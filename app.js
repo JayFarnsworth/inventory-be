4a52e0d277bd926690889a2e43c4c20b693f0014
Jay "Danger" Farnsworth <DrDanger@Jays-MacBook-Pro.local>
2018-07-12 10:31:48 -0600

    modified express boilerplate

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var http = require('http');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
 
 var app = express();
 
