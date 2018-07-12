1add19186332596abec689d540843ab80facb9a7
Jay "Danger" Farnsworth <DrDanger@Jays-MacBook-Pro.local>
2018-07-12 13:44:28 -0600

    working basic mongo router

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var http = require('http');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var request = require('request');
 
 
 var app = express();
+var api = require('./controllers/api.js')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,8 +20,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+app.use('/', api)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,7 +38,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  // res.render('error');
 });
 
 module.exports = app;
---
+++ controllers/api.js
@@ -0,0 +1,39 @@
+const routes = require('express').Router();
+const mongo = './database/mongo.js';
+const fetch = require('node-fetch');
+const request = require('request');
+
+
+routes.get('/hello', (req, res) => {
+  console.log('hi')
+  res.send('hello')
+});
+
+routes.get('/users', (req, res) => {
+  console.log('mongo working')
+  var name = req.query.name;
+  fetch(`http://localhost:4000/users?name=${name}`)
+    .then(resp => resp.json())
+    .then(resp => {res.send(resp)})
+})
+
+routes.post('/users/post', (req, res) => {
+  var clientServerOptions = {
+    uri: 'http://localhost:4000/users/post',
+    body: JSON.stringify(req.body),
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+  request(clientServerOptions, function (error, response) {
+    console.log(response.body);
+    res.send(response.body)
+    return;
+  });
+})
+
+
+
+
+module.exports = routes;
